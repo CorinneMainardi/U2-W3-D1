cecf22d55905017c4c482738b7a29522953cab1c
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-09-23 14:08:40 +0200

    creata classe pets

--- script.js
+++ script.js
@@ -20,7 +20,22 @@ const user2 = new User("Jonathan", "Mainardi", 29, "Terni");
 
 console.log(user1.older(user2));
 
+class Pets {
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+  }
+  sameOwner(otherPet) {
+    if (this.ownerName === otherPet.ownerName) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
-
-
-class
+const jamie = new Pets("jamie", "Jonathan", "cane", "dogo");
+const leo = new Pets("leonida", "Jonathan", "cane", "dogo");
+const sarabi = new Pets("sarabi", "corinne", "cane", "america bully");
